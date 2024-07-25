7e923106f329b896ddb52e729591fc1334ac6ef7
Abitha <18137006+abitha-thankaraj@users.noreply.github.com>
2024-07-24 21:23:24 -0400

    Fastchat (#1)
    * ignore logs
    
    * cleanup
    
    * skeleton code to deploy and run chat completion
    
    * chat completion example
    
    ---------
    
    Co-authored-by: abitha <athanka@cs.cmu.edu>

---
+++ redteam/chat_completion.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+import requests
+import json
+
+
+@dataclass
+class ChatCompletionConfig:
+    url: str = "http://localhost:8003/v1/chat/completions"
+    model: str = "Mistral-7B-Instruct-v0.2"
+
+
+
+
+class ChatCompletion:
+    def __init__(self, config: ChatCompletionConfig):
+        self.config = config
+
+    def create_message(prompt:str):
+        return {"role": "user", "content": prompt}
+
+    def call_chat_completion(self, messages: list) -> dict:
+        url = self.config.url
+        headers = {
+            "Content-Type": "application/json"
+        }
+        data = {
+            "model": self.config.model,
+            "messages": messages
+        }
+        
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return {"error": response.status_code, "message": response.text}
+
+if __name__ == "__main__":
+    messages = [{"role": "user", "content": "Hello! What is your name?"}]
+    config = ChatCompletionConfig()
+
+    chat_completion = ChatCompletion(config)
+    response = chat_completion.call_chat_completion(messages)
+    print(response)
+    
--- scripts/deploy_api.sh
+++ scripts/deploy_api.sh
@@ -11,5 +11,5 @@ export LOGDIR="/data/tir/projects/tir7/user_data/athankar/redteaming/scripts/log
 python3 -m fastchat.serve.controller &
 # Starting the model worker with the specified model path
 python3 -m fastchat.serve.model_worker --model-path mistralai/Mistral-7B-Instruct-v0.2 &
-# Add model-list-mode because you're running the model-worker in the background and model loading takes time and does not 
-python3 -m fastchat.serve.gradio_web_server  --host 0.0.0.0 --port 8001 --model-list-mode=reload
+
+python3 -m fastchat.serve.openai_api_server --host 0.0.0.0 --port 8003
