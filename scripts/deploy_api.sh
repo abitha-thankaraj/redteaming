9f925f18ed333f6871710c01994b65af16ccbe11
abitha <athanka@cs.cmu.edu>
2024-07-27 16:33:52 -0400

    deploy server

--- scripts/deploy_api.sh
+++ scripts/deploy_api.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-source /home/ftajwar/.bashrc
-source /home/ftajwar/anaconda3/etc/profile.d/conda.sh
-conda activate fastchat
+source /home/athankar/.bashrc
+source /home/athankar/miniconda3/etc/profile.d/conda.sh
+conda activate redteam
 
-# export HF_HOME="/data/tir/projects/tir6/bisk/athankar/projects/.cache";
-# export LOGDIR="/data/tir/projects/tir7/user_data/athankar/redteaming/scripts/logs/$(date +'%Y-%m-%d-%H-%M-%S-%3N')"
-export TRANSFORMERS_CACHE=/data/user_data/ftajwar/training_cache
-export HF_HOME=/data/user_data/ftajwar/training_cache
+export HF_HOME="/data/tir/projects/tir6/bisk/athankar/projects/.cache";
+export LOGDIR="/data/tir/projects/tir7/user_data/athankar/redteaming/scripts/logs/$(date +'%Y-%m-%d-%H-%M-%S-%3N')"
 
 # Starting the controller
 python3 -m fastchat.serve.controller &
 # Starting the model worker with the specified model path
-python3 -m fastchat.serve.model_worker --model-path mistralai/Mistral-7B-Instruct-v0.1 &
+python3 -m fastchat.serve.model_worker --model-path $1 &
 
-python3 -m fastchat.serve.openai_api_server --host 0.0.0.0 --port 8003
+python3 -m fastchat.serve.openai_api_server --host 0.0.0.0 --port $2
+
+#TODO: Write a script to ensure model is loaded; something short to ping the server for that.
