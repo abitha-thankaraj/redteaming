f647b3bd6b0b3ceb72c05e17d2403109050a0609
Fahim Tajwar <fahimtajwar@Fahims-MacBook-Pro.local>
2024-07-27 16:24:43 -0400

    preliminary datasets: openai and harmbench

--- redteam/data_generation_pipeline/attack_prompts_dataset.py
+++ redteam/data_generation_pipeline/attack_prompts_dataset.py
@@ -18,3 +18,44 @@ def get_openai_redteaming_dataset(dataset_path: str) -> ArrowDataset:
         field="Questions", 
         split='train',
     )
+
+
+def get_harmbench_dataset(dataset_path: str) -> ArrowDataset:
+    """
+    Loads the 400 prompts from the HarmBench dataset.
+    Paper: https://arxiv.org/abs/2402.04249
+    Data: https://github.com/centerforaisafety/HarmBench
+
+    Example Usage:
+        dataset = get_harmbench_dataset(dataset_path)
+        for i in range(len(dataset)):
+            print(dataset[i]['prompt'])
+    """
+    return load_dataset(
+        "csv", 
+        data_files=dataset_path, 
+        split="train",
+    ).rename_column("Behavior", "prompt")
+
+
+def get_dataset(
+    dataset_name: str,
+    dataset_path: str,
+) -> ArrowDataset:
+    """
+    Wrapper function unifying all the individual dataset loading functions
+    """
+
+    dataset_loading_functions = {
+        "openai": get_openai_redteaming_dataset,
+        "harmbench": get_harmbench_dataset,
+    }
+
+    dataset_loading_kwargs = {
+        "dataset_path": dataset_path,
+    }
+
+    if dataset_name not in dataset_loading_functions:
+        raise ValueError(f"Give dataset {dataset_name} not supported.")
+
+    return dataset_loading_functions[dataset_name](**dataset_loading_kwargs)
