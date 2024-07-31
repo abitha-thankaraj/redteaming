4513a7090bf4986042f8215bef30ee15ef95cf64
Fahim Tajwar <tajwarfahim932@gmail.com>
2024-07-31 18:22:58 -0400

    more in context examples

---
+++ redteam/attack_prompts_dataset.py
@@ -0,0 +1,74 @@
+from datasets import load_dataset
+from datasets.arrow_dataset import Dataset as ArrowDataset
+
+
+def get_openai_redteaming_dataset(dataset_path: str) -> ArrowDataset:
+    """
+    Loads the 15 (the actual dataset contains 16 examples, we omit one) prompts
+    from GPT-4 Technical Report, used to redteam the original GPT-4 model.
+    Example Usage:
+        dataset = get_openai_redteaming_dataset(dataset_path)
+        for i in range(len(dataset)):
+            print(dataset[i]['prompt'])
+    """
+    return load_dataset(
+        "json",
+        data_files=dataset_path,
+        field="Questions",
+        split="train",
+    )
+
+
+def get_harmbench_dataset(dataset_path: str) -> ArrowDataset:
+    """
+    Loads the 400 prompts from the HarmBench dataset.
+    Paper: https://arxiv.org/abs/2402.04249
+    Data: https://github.com/centerforaisafety/HarmBench
+    Example Usage:
+        dataset = get_harmbench_dataset(dataset_path)
+        for i in range(len(dataset)):
+            print(dataset[i]['prompt'])
+    """
+    return load_dataset(
+        "csv",
+        data_files=dataset_path,
+        split="train",
+    ).rename_column("Behavior", "prompt")
+
+
+def get_advbench_dataset(dataset_path: str) -> ArrowDataset:
+    """
+    Loads the 500 harmful prompts from the AdvBench dataset.
+    Paper: https://arxiv.org/abs/2307.15043
+    Data: https://github.com/llm-attacks/llm-attacks/tree/main/data/advbench
+    Example Usage:
+        dataset = get_advbench_dataset(dataset_path)
+        for i in range(len(dataset)):
+            print(dataset[i]['prompt'])
+    """
+    return load_dataset("csv", data_files=dataset_path, split="train").rename_column(
+        "goal", "prompt"
+    )
+
+
+def get_dataset(
+    dataset_name: str,
+    dataset_path: str,
+) -> ArrowDataset:
+    """
+    Wrapper function unifying all the individual dataset loading functions
+    """
+    dataset_loading_functions = {
+        "openai": get_openai_redteaming_dataset,
+        "harmbench": get_harmbench_dataset,
+        "advbench": get_advbench_dataset,
+    }
+
+    dataset_loading_kwargs = {
+        "dataset_path": dataset_path,
+    }
+
+    if dataset_name not in dataset_loading_functions:
+        raise ValueError(f"Give dataset {dataset_name} not supported.")
+
+    return dataset_loading_functions[dataset_name](**dataset_loading_kwargs)
--- redteam/generate_multiturn_attack_prompts.py
+++ redteam/generate_multiturn_attack_prompts.py
@@ -51,34 +51,34 @@ def main(config: DictConfig):
     all_multiturn_attacks = []
 
     for i in tqdm(range(len(harmful_questions_dataset))):
-        random_index = np.random.choice(a=len(in_context_example_prompts))
-        in_context_example = in_context_example_prompts[random_index]
-        harmful_question = harmful_questions_dataset[i]["prompt"]
+        for index in range(len(in_context_example_prompts)):
+            in_context_example = in_context_example_prompts[index]
+            harmful_question = harmful_questions_dataset[i]["prompt"]
 
-        user_message = user_message_template.format(
-            in_context_examples=in_context_example,
-            goal=harmful_question,
-            num_turns=config.num_turns,
-        )
-
-        all_valid_questions = False
-        while not all_valid_questions:
-            response = chat_completion.multiturn_chat_completion(
-                system_prompt=system_prompt, messages=[user_message]
-            )
-            all_questions = parse_multiturn_attack_prompt(
-                chat_completion_dict=response[-1],
+            user_message = user_message_template.format(
+                in_context_examples=in_context_example,
+                goal=harmful_question,
                 num_turns=config.num_turns,
             )
-            all_valid_questions = all([is_valid_question(question=all_questions[key]) for key in all_questions])
 
-        attack_output_dict = {
-            "goal": harmful_question,
-            "response": response,
-            "questions": all_questions,
-        }
+            all_valid_questions = False
+            while not all_valid_questions:
+                response = chat_completion.multiturn_chat_completion(
+                    system_prompt=system_prompt, messages=[user_message]
+                )
+                all_questions = parse_multiturn_attack_prompt(
+                    chat_completion_dict=response[-1],
+                    num_turns=config.num_turns,
+                )
+                all_valid_questions = all([is_valid_question(question=all_questions[key]) for key in all_questions])
 
-        all_multiturn_attacks.append(attack_output_dict)
+            attack_output_dict = {
+                "goal": harmful_question,
+                "response": response,
+                "questions": all_questions,
+            }
+
+            all_multiturn_attacks.append(attack_output_dict)
 
         if i % 10 == 0:
             os.makedirs(config.out_dir, exist_ok=True)
