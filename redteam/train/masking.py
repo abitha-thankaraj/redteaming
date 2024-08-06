a838114bd079bd1f664462c4cc2ddcc325ca9894
abitha <athanka@cs.cmu.edu>
2024-08-06 13:42:16 -0400

    masking

---
+++ redteam/train/masking.py
@@ -0,0 +1,143 @@
+from typing import Any, Tuple, Dict, List
+from transformers import AutoTokenizer
+from dataclasses import dataclass
+import transformers
+
+
+@dataclass
+class TokenizerSeparators:
+    assistant_prefix: str = ("",)
+    assistant_suffix: str = ("",)
+    prefix_offset: int = (0,)  # Offset to add for masking
+
+
+def get_tokenizer_separators(
+    tokenizer: AutoTokenizer,
+) -> Tuple[AutoTokenizer, TokenizerSeparators]:
+    if tokenizer.name_or_path == "meta-llama/Meta-Llama-3.1-8B-Instruct":
+        tokenizer.unk_token_id = 128004  # Use the finetune right padding token
+        tokenizer.pad_token_id = 128004  # Use the finetune right padding token
+        return tokenizer, TokenizerSeparators(
+            assistant_prefix="<|start_header_id|>assistant<|end_header_id|>\n\n",
+            assistant_suffix="<|eot_id|>",
+            prefix_offset=0,
+        )
+    elif tokenizer.name_or_path == "mistralai/Mistral-7B-Instruct-v0.1":
+        tokenizer.pad_token_id = tokenizer.unk_token_id
+        return tokenizer, TokenizerSeparators(
+            assistant_prefix=" [/INST] ", 
+            assistant_suffix="</s>", 
+            prefix_offset=-1
+        )
+    else:
+        raise ValueError(f"Tokenizer {tokenizer.name_or_path} not supported")
+
+
+def mask_non_assistant_tokens(
+    tokenizer: AutoTokenizer,
+    conversation: List[Dict[str, str]],
+    tokenizer_separator: TokenizerSeparators,
+):
+    tokens = tokenizer.apply_chat_template(
+        conversation, tokenize=True, padding="max_length", add_special_tokens=False
+    )
+    print(tokenizer.decode(tokens, add_special_tokens=False))
+    masked_tokens = [tokenizer.pad_token_id] * len(tokens)  # Start with all tokens masked
+    assistant_prefix_ids = tokenizer.encode(
+        tokenizer_separator.assistant_prefix, add_special_tokens=False
+    )
+    assistant_contents = [
+        tokenizer_separator.assistant_prefix
+        + msg["content"]
+        + tokenizer_separator.assistant_suffix
+        for msg in conversation
+        if msg["role"] == "assistant"
+    ]
+
+    start_index = 0
+    for content in assistant_contents:
+        content_tokens = tokenizer.encode(content, add_special_tokens=False)
+        # print(content_tokens)
+        # print(tokenizer.decode(content_tokens))
+        while True:
+            try:
+                start_index = tokens.index(content_tokens[0], start_index)
+                if tokens[start_index : start_index + len(content_tokens)] == content_tokens:
+                    masked_tokens[
+                        start_index
+                        + len(assistant_prefix_ids)
+                        + tokenizer_separator.prefix_offset : start_index
+                        + len(content_tokens)
+                    ] = tokens[
+                        start_index
+                        + len(assistant_prefix_ids)
+                        + tokenizer_separator.prefix_offset : start_index
+                        + len(content_tokens)
+                    ]
+                    break
+                start_index += 1
+            except ValueError:
+                break
+    return masked_tokens
+    # return dict(
+    #     input_ids=tokens,
+    #     labels=masked_tokens,
+    #     attention_mask=tokens.ne(tokenizer.pad_token_id),
+    # )
+
+
+if __name__ == "__main__":
+    conversation = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Hello, how are you?"},
+        {
+            "role": "assistant",
+            "content": "I'm doing well, thank you! How can I assist you today?",
+        },
+        {"role": "user", "content": "What's the weather like today?"},
+        {"role": "assistant", "content": "How can I assist you tomorrow?"},
+    ]
+
+    model_name = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+    config = transformers.AutoConfig.from_pretrained(
+        model_name,
+        trust_remote_code=True,
+    )
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
+        model_name,
+        config=config,
+        trust_remote_code=True,
+        model_max_length=64,
+        padding_side="right",
+        use_fast=False,
+    )
+    tokenizer, tokenizer_separator = get_tokenizer_separators(tokenizer)
+    masked_tokens = mask_non_assistant_tokens(tokenizer, conversation, tokenizer_separator)
+    print("Meta-Llama")
+    print(tokenizer.decode(masked_tokens).replace(tokenizer.pad_token, ""))
+
+    mistral_config = transformers.AutoConfig.from_pretrained(
+        "mistralai/Mistral-7B-Instruct-v0.1",
+        trust_remote_code=True,
+    )
+    mistral_tokenizer = transformers.AutoTokenizer.from_pretrained(
+        "mistralai/Mistral-7B-Instruct-v0.1",
+        config=mistral_config,
+        trust_remote_code=True,
+        model_max_length=64,
+        padding_side="right",
+        use_fast=False,
+    )
+
+    mistral_tokenizer, mistral_tokenizer_separator = get_tokenizer_separators(
+        mistral_tokenizer
+    )
+    mistral_masked_tokens = mask_non_assistant_tokens(
+        mistral_tokenizer, conversation, mistral_tokenizer_separator
+    )
+    print("Mistral")
+    print(
+        mistral_tokenizer.decode(mistral_masked_tokens).replace(
+            mistral_tokenizer.pad_token, ""
+        )
+    )
