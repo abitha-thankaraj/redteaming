masking fixes

redteam/train/masking.py
@@ -3,13 +3,21 @@ from typing import Any, Tuple, Dict, List
 from transformers import AutoTokenizer
 from dataclasses import dataclass
 import transformers
+from transformers.trainer_pt_utils import LabelSmoother
 
+IGNORE_TOKEN_ID = LabelSmoother.ignore_index
 
 @dataclass
 class TokenizerSeparators:
     assistant_prefix: str = "",
     assistant_suffix: str = "",
     prefix_offset: int = 0,  # Offset to add for masking; Ugly single space with mistral.
+    assistant_prefix_ids: List[int] = None
+
+    def set_assistant_prefix_ids(self, tokenizer: AutoTokenizer):
+        self.assistant_prefix_ids = tokenizer.encode(
+            self.assistant_prefix, add_special_tokens=False
+        )
 
 
 def get_tokenizer_separators(
@@ -18,18 +26,22 @@ def get_tokenizer_separators(
     if tokenizer.name_or_path == "meta-llama/Meta-Llama-3.1-8B-Instruct":
         tokenizer.unk_token_id = 128004  # Use the finetune right padding token
         tokenizer.pad_token_id = 128004  # Use the finetune right padding token
-        return tokenizer, TokenizerSeparators(
+        tokenizer_separator = TokenizerSeparators(
             assistant_prefix="<|start_header_id|>assistant<|end_header_id|>\n\n",
             assistant_suffix="<|eot_id|>",
             prefix_offset=0,
         )
+        tokenizer_separator.set_assistant_prefix_ids(tokenizer)
+        return tokenizer, tokenizer_separator
     elif tokenizer.name_or_path == "mistralai/Mistral-7B-Instruct-v0.1":
         tokenizer.pad_token_id = tokenizer.unk_token_id
-        return tokenizer, TokenizerSeparators(
+        tokenizer_separator = TokenizerSeparators(
             assistant_prefix=" [/INST] ", 
             assistant_suffix="</s>", 
             prefix_offset=-1
         )
+        tokenizer_separator.set_assistant_prefix_ids(tokenizer)
+        return tokenizer, tokenizer_separator
     else:
         raise ValueError(f"Tokenizer {tokenizer.name_or_path} not supported")
 
@@ -39,13 +51,15 @@ def mask_non_assistant_tokens(
     conversation: List[Dict[str, str]],
     tokenizer_separator: TokenizerSeparators,
 ):
+
     tokens = tokenizer.apply_chat_template(
         conversation, tokenize=True, padding="max_length", truncation=True
     )
-    masked_tokens = [tokenizer.pad_token_id] * len(tokens)  # Start with all tokens masked
-    assistant_prefix_ids = tokenizer.encode(
-        tokenizer_separator.assistant_prefix, add_special_tokens=False
-    )
+    # masked_tokens = [tokenizer.pad_token_id] * len(tokens)  # Start with all tokens masked
+    masked_tokens = [IGNORE_TOKEN_ID] * len(tokens)  # Start with all tokens masked
+    assert tokenizer_separator.assistant_prefix_ids is not None, "Assistant prefix ids not set"
+    assistant_prefix_ids = tokenizer_separator.assistant_prefix_ids
+
     assistant_contents = [
         tokenizer_separator.assistant_prefix
         + msg["content"]
@@ -86,6 +100,12 @@ def mask_non_assistant_tokens(
 
 
 if __name__ == "__main__":
+
+    EXAMPLE_FNAME = "/data/group_data/rl/datasets/redteaming/gen_judge_multiturn_conversation_chunked/gpt-4_judge_generated_multiturn_conversations_22-18-1723083523_input_Mistral-7B-Instruct-v0.1_test_evaluated_multiturn_responses_16-55-1722459320.json"
+    from redteam.train.datasets import get_conversations
+    attacker_raw_messages = get_conversations(EXAMPLE_FNAME, "attacker")
+
+    c = attacker_raw_messages[0]
     conversation = [
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "Hello, how are you?"},
@@ -102,19 +122,22 @@ if __name__ == "__main__":
         model_name,
         trust_remote_code=True,
     )
+    
     tokenizer = transformers.AutoTokenizer.from_pretrained(
         model_name,
         config=config,
         trust_remote_code=True,
-        model_max_length=64,
+        # model_max_length=1024,
         padding_side="right",
         use_fast=False,
     )
     tokenizer, tokenizer_separator = get_tokenizer_separators(tokenizer)
+    from IPython import embed; embed()
+
     masked_tokens = mask_non_assistant_tokens(tokenizer, conversation, tokenizer_separator)["labels"]
     print("Meta-Llama")
-    print(tokenizer.decode(masked_tokens).replace(tokenizer.pad_token, ""))
-
+    print(tokenizer.decode(torch.where(masked_tokens == IGNORE_TOKEN_ID, tokenizer.pad_token_id, masked_tokens)).replace(tokenizer.pad_token, ""))
+    
     mistral_config = transformers.AutoConfig.from_pretrained(
         "mistralai/Mistral-7B-Instruct-v0.1",
         trust_remote_code=True,
@@ -135,8 +158,10 @@ if __name__ == "__main__":
         mistral_tokenizer, conversation, mistral_tokenizer_separator
     )["labels"]
     print("Mistral")
-    print(
-        mistral_tokenizer.decode(mistral_masked_tokens).replace(
-            mistral_tokenizer.pad_token, ""
-        )
-    )
+    print(mistral_tokenizer.decode(torch.where(mistral_masked_tokens == IGNORE_TOKEN_ID, mistral_tokenizer.pad_token_id, mistral_masked_tokens)).replace(mistral_tokenizer.pad_token, ""))
+
+    # print(
+    #     mistral_tokenizer.decode(mistral_masked_tokens).replace(
+    #         mistral_tokenizer.pad_token, ""
+    #     )
+    # )

scripts/data_generation/judge_multiturn_conversation.py
@@ -28,12 +28,14 @@ from redteam.utils.hash_utils import get_question_id_sha256
 
 
 def rename_savefile(save_file, input_file):
-    input_file = input_file.split("/")[-1]
-    save_file = save_file.replace(".json", f"_input_{input_file}")
+    save_file = save_file.replace(".json", "_input_{input_fname}".format(input_fname = input_file.split("/")[-1]))
     return save_file
 
 def get_generation_model_name(input_file):
     return input_file.split("/")[-1].split("_")[0]
+
+def get_prompt_dataset(input_file):
+    return input_file.split("/")[-1].split("_")[1]
 
 
 
@@ -48,6 +50,10 @@ def main(config: DictConfig):
     Follows the method of this paper: Leveraging the Context through Multi-Round Interactions for Jailbreaking Attacks (https://arxiv.org/abs/2402.09177)
     """
     config.repo_dir = PARENT_DIR
+    
+    config.prompt_dataset = get_prompt_dataset(config.multiturn_conversations_fname)
+    config.save_file = rename_savefile(config.save_file, config.multiturn_conversations_fname)
+
     OmegaConf.resolve(config)
     np.random.seed(config.seed)
 
@@ -55,8 +61,7 @@ def main(config: DictConfig):
     if missing_keys:
         raise ValueError(f"Got missing keys in config:\n{missing_keys}")
     os.makedirs(config.out_dir, exist_ok=True)
-    config.save_file = rename_savefile(config.save_file, config.multiturn_conversations_fname)
-
+    
     generation_model = get_generation_model_name(
         input_file=config.multiturn_conversations_fname,
     )