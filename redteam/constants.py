97cfdaa0929c3f08a7c57758f88fa679b751dc40
abitha <athanka@cs.cmu.edu>
2024-08-07 22:04:15 -0400

    dirs

--- redteam/constants.py
+++ redteam/constants.py
@@ -9,7 +9,7 @@ SCRIPTS_DIR = os.path.join(PARENT_DIR, "scripts")
 EXPERIMENT_SCRIPTS_DIR = os.path.join(SCRIPTS_DIR, "experiments")
 EXPERIMENT_LOG_DIR = os.path.join(SCRIPTS_DIR, "logs")
 EXPERIMENT_CONFIG_DIR = os.path.join(EXPERIMENT_SCRIPTS_DIR, "configs")
-DATAGEN_CONFIG_DIR = os.path.join(EXPERIMENT_CONFIG_DIR, "data_generation")
+DATAGEN_CONFIG_DIR = os.path.join(SCRIPTS_DIR, "data_generation")
 
 
 WANDB_DIR = EXPERIMENT_LOG_DIR
