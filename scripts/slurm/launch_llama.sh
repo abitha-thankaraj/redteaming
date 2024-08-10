c2500834523a9a4bf4a381c0566f39614c0001c8
Fahim Tajwar <fahimtajwar@Fahims-MacBook-Pro.local>
2024-08-10 17:00:25 -0400

    use_special_tokens is added as a parameter in the config

--- scripts/slurm/launch_llama.sh
+++ scripts/slurm/launch_llama.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH --job-name=llama_$SLURM_ARRAY_TASK_ID
-#SBATCH --output=/home/athankar/slurm/%A_%a.out
-#SBATCH --error=/home/athankar/slurm/%A_%a.err
+#SBATCH --output=/home/ftajwar/slurm/%A_%a.out
+#SBATCH --error=/home/ftajwar/slurm/%A_%a.err
 #SBATCH --time=6:00:00
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=16G
 #SBATCH --partition=general
 #SBATCH --gres=gpu:A6000:1
-#SBATCH --mail-user=athankar@cs.cmu.edu
+#SBATCH --mail-user=ftajwar@cs.cmu.edu
 #SBATCH --mail-type=END,FAIL
 #SBATCH --array=0-8
 
@@ -18,19 +18,9 @@ OPENAI_DIR="/data/group_data/rl/datasets/redteaming/gen_multiturn_prompts/openai
 ADVBENCH_DIR="/data/group_data/rl/datasets/redteaming/gen_multiturn_prompts/advbench_chunked"
 
 # Find all files that match the pattern in both directories and store them as an array
-# FILES=($(find $HARMBENCH_DIR $OPENAI_DIR -type f -name "*.json"))
-FILES=($(find $ADVBENCH_DIR -type f -name "*.json"))
+FILES=($(find $HARMBENCH_DIR $OPENAI_DIR $ADVBENCH_DIR-type f -name "*.json"))
+# FILES=($(find $ADVBENCH_DIR -type f -name "*.json"))
 
-MISSING_FNAMES=(
-    "$ADVBENCH_DIR/gpt-3.5-turbo-0125_advbench_generated_multiturn_prompts_20-10-1722471013_1700_1800.json"
- "$ADVBENCH_DIR/gpt-3.5-turbo-0125_advbench_generated_multiturn_prompts_20-10-1722471013_3000_3100.json"
- "$ADVBENCH_DIR/gpt-3.5-turbo-0125_advbench_generated_multiturn_prompts_20-10-1722471013_3100_3200.json"
- "$ADVBENCH_DIR/gpt-3.5-turbo-0125_advbench_generated_multiturn_prompts_20-10-1722471013_3400_3500.json"
- "$ADVBENCH_DIR/gpt-3.5-turbo-0125_advbench_generated_multiturn_prompts_20-10-1722471013_3500_3600.json"
- "$ADVBENCH_DIR/gpt-3.5-turbo-0125_advbench_generated_multiturn_prompts_20-10-1722471013_3600_3700.json"
- "$ADVBENCH_DIR/gpt-3.5-turbo-0125_advbench_generated_multiturn_prompts_20-10-1722471013_3800_3900.json"
- "$ADVBENCH_DIR/gpt-3.5-turbo-0125_advbench_generated_multiturn_prompts_20-10-1722471013_600_700.json"
-)
 
 # Variables
 FAST_CHAT_MODEL_PATH="meta-llama/Meta-Llama-3.1-8B-Instruct";
@@ -38,7 +28,7 @@ FAST_CHAT_API_PORT=$((11003 + SLURM_ARRAY_TASK_ID))
 CONTROLLER_PORT=$((12000 + SLURM_ARRAY_TASK_ID))
 WORKER_PORT=$((13000 + SLURM_ARRAY_TASK_ID))
 
-REPO_DIR="/data/tir/projects/tir7/user_data/athankar/redteaming"; #TODO: FAHIM
+REPO_DIR="/home/ftajwar/redteaming"; #TODO: FAHIM
 CHAT_COMPLETION_MODEL="llama-3.1-8b-instruct.yaml";
 
 # Navigate to the script directory
@@ -46,4 +36,4 @@ cd $REPO_DIR/scripts/slurm/
 
 # Deploy and infer
 # ./deploy_and_infer.sh $FAST_CHAT_MODEL_PATH $FAST_CHAT_API_PORT $REPO_DIR $CHAT_COMPLETION_MODEL "${FILES[$SLURM_ARRAY_TASK_ID]}" $CONTROLLER_PORT $WORKER_PORT
-./deploy_and_infer.sh $FAST_CHAT_MODEL_PATH $FAST_CHAT_API_PORT $REPO_DIR $CHAT_COMPLETION_MODEL "${MISSING_FNAMES[$SLURM_ARRAY_TASK_ID]}" $CONTROLLER_PORT $WORKER_PORT
+./deploy_and_infer_ft.sh $FAST_CHAT_MODEL_PATH $FAST_CHAT_API_PORT $REPO_DIR $CHAT_COMPLETION_MODEL "${MISSING_FNAMES[$SLURM_ARRAY_TASK_ID]}" $CONTROLLER_PORT $WORKER_PORT
--- scripts/slurm/launch_mistral.sh
+++ scripts/slurm/launch_mistral.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH --job-name=mistral_$SLURM_ARRAY_TASK_ID
-#SBATCH --output=/home/athankar/slurm/%A_%a.out
-#SBATCH --error=/home/athankar/slurm/%A_%a.err
+#SBATCH --output=/home/ftajwar/slurm/%A_%a.out
+#SBATCH --error=/home/ftajwar/slurm/%A_%a.err
 #SBATCH --time=6:00:00
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=16G
 #SBATCH --partition=general
 #SBATCH --gres=gpu:A6000:1
-#SBATCH --mail-user=athankar@cs.cmu.edu
+#SBATCH --mail-user=ftajwar@cs.cmu.edu
 #SBATCH --mail-type=END,FAIL
 #SBATCH --array=0-8
 
@@ -19,18 +19,8 @@ ADVBENCH_DIR="/data/group_data/rl/datasets/redteaming/gen_multiturn_prompts/advb
 
 # Find all files that match the pattern in both directories and store them as an array
 # FILES=($(find $HARMBENCH_DIR $OPENAI_DIR -type f -name "*.json"))
-FILES=($(find $ADVBENCH_DIR -type f -name "*.json"))
-
-MISSING_FNAMES=(
-    "$ADVBENCH_DIR/gpt-3.5-turbo-0125_advbench_generated_multiturn_prompts_20-10-1722471013_1700_1800.json"
- "$ADVBENCH_DIR/gpt-3.5-turbo-0125_advbench_generated_multiturn_prompts_20-10-1722471013_3000_3100.json"
- "$ADVBENCH_DIR/gpt-3.5-turbo-0125_advbench_generated_multiturn_prompts_20-10-1722471013_3100_3200.json"
- "$ADVBENCH_DIR/gpt-3.5-turbo-0125_advbench_generated_multiturn_prompts_20-10-1722471013_3400_3500.json"
- "$ADVBENCH_DIR/gpt-3.5-turbo-0125_advbench_generated_multiturn_prompts_20-10-1722471013_3500_3600.json"
- "$ADVBENCH_DIR/gpt-3.5-turbo-0125_advbench_generated_multiturn_prompts_20-10-1722471013_3600_3700.json"
- "$ADVBENCH_DIR/gpt-3.5-turbo-0125_advbench_generated_multiturn_prompts_20-10-1722471013_3800_3900.json"
- "$ADVBENCH_DIR/gpt-3.5-turbo-0125_advbench_generated_multiturn_prompts_20-10-1722471013_600_700.json"
-)
+FILES=($(find $HARMBENCH_DIR $OPENAI_DIR $ADVBENCH_DIR-type f -name "*.json"))
+# FILES=($(find $ADVBENCH_DIR -type f -name "*.json"))
 
 # Variables
 FAST_CHAT_MODEL_PATH="mistralai/Mistral-7B-Instruct-v0.1";
@@ -38,7 +28,7 @@ FAST_CHAT_API_PORT=$((10003 + SLURM_ARRAY_TASK_ID))
 CONTROLLER_PORT=$((27000 + SLURM_ARRAY_TASK_ID))
 WORKER_PORT=$((26000 + SLURM_ARRAY_TASK_ID))
 
-REPO_DIR="/data/tir/projects/tir7/user_data/athankar/redteaming"; #TODO: FAHIM
+REPO_DIR="/home/ftajwar/redteaming"; #TODO: FAHIM
 CHAT_COMPLETION_MODEL="mistral-7b-instruct-v0.1.yaml";
 
 # Navigate to the script directory
@@ -46,4 +36,4 @@ cd $REPO_DIR/scripts/slurm/
 
 # Deploy and infer
 # ./deploy_and_infer.sh $FAST_CHAT_MODEL_PATH $FAST_CHAT_API_PORT $REPO_DIR $CHAT_COMPLETION_MODEL "${FILES[$SLURM_ARRAY_TASK_ID]}" $CONTROLLER_PORT $WORKER_PORT
-./deploy_and_infer.sh $FAST_CHAT_MODEL_PATH $FAST_CHAT_API_PORT $REPO_DIR $CHAT_COMPLETION_MODEL "${MISSING_FNAMES[$SLURM_ARRAY_TASK_ID]}" $CONTROLLER_PORT $WORKER_PORT
+./deploy_and_infer_ft.sh $FAST_CHAT_MODEL_PATH $FAST_CHAT_API_PORT $REPO_DIR $CHAT_COMPLETION_MODEL "${MISSING_FNAMES[$SLURM_ARRAY_TASK_ID]}" $CONTROLLER_PORT $WORKER_PORT
