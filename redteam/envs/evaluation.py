e6fb7f7ca3dc0e78de64330c2b082f7b4af7580b
abitha <athanka@cs.cmu.edu>
2024-09-13 13:16:17 -0400

    added value function accuracy evals

--- redteam/envs/evaluation.py
+++ redteam/envs/evaluation.py
@@ -1,12 +1,7 @@
-import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
 from gym.utils import seeding
 from typing import Optional, Any
 from redteam.envs.common import GameConversation
-import logging
-
-logger = logging.getLogger(__name__)
-
+from redteam.train.datasets import get_value_function_keywords, get_reward_to_gos
 
 class Game:
     def __init__(self, seed, attacker, defender, judge, goals=None, max_turns=3):
@@ -38,11 +33,8 @@ class Game:
         return self._env_goal
 
     def simulate_one_turn(self):
-        print(f"Attacker message: {self.env_state.to_attacker_message()}")
         attacker_action = self.attacker.act(self.env_state.to_attacker_message())
         self.env_state.messages.append(("attacker", attacker_action))
-        print("*********************************************************")
-        print(f"Defender message: {self.env_state.to_defender_message()}")
         defender_action = self.defender.act(self.env_state.to_defender_message())
         self.env_state.messages.append(("defender", defender_action))
         self._turn += 1
@@ -54,7 +46,36 @@ class Game:
     def simulate(self):
         while self._turn < self.max_turns:
             self.simulate_one_turn()
-        traj = self.env_state.to_game_message()
+        # traj = self.env_state.to_game_message()
         # Remove the goal. Pass in as openai format for judge
         judge_score = self.judge.score(self.env_state.to_defender_message())
-        return traj, judge_score
+        return self.env_state, judge_score
+
+def evaluate_value_function(conversation, value_function_type, rewards, policy_type):
+    if policy_type == "defender":
+        # flip rewards
+        rewards = [1.0 - r for r in rewards]
+    # Calculate the ground truth value function
+    if value_function_type in ["binary", "natural_lang_binary"]:
+        rews = rewards
+    elif value_function_type in ["multilabel", "natural_lang_multiclass"]:
+        rews = get_reward_to_gos(rewards, gamma=0.9)
+    else:
+        raise NotImplementedError(f"Value function type {value_function_type} not implemented")
+
+    value_function_token_map = get_value_function_keywords(value_function_type, 
+                                                           gamma=0.9, 
+                                                           model_name="meta-llama/Meta-Llama-3.1-8B-Instruct")
+    gt_vfs = []
+    is_correct_value_function_predictions = []
+    for i, rew in enumerate(rews):
+        gt_vfs.append(value_function_token_map[rew])
+        # get assistant turn
+        assert conversation[i*2+1]["role"] == "assistant", "Assistant turn not found"
+        # Did the assistant predict the correct value function for future states?
+        is_correct_value_function_predictions.append(conversation[i*2+1]["content"].strip().startswith(value_function_token_map[rew]))
+
+    return {
+        "ground_truth_value_functions": gt_vfs,
+        "is_correct_value_function_predictions": is_correct_value_function_predictions
+    }
--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -11,13 +11,15 @@ from redteam.utils.data_utils import read_json, write_json
 from redteam.data_generation.attack_prompts_dataset import get_dataset
 from redteam.utils.slack_me import slack_notification
 from hydra.core.hydra_config import HydraConfig
-from redteam.envs.evaluation import Game
+from redteam.envs.evaluation import Game, evaluate_value_function
+from redteam.train.common import set_seed_everywhere
 
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 global_config = None
+
 
 @hydra.main(
     version_base=None,
@@ -29,10 +31,10 @@ def main(config: DictConfig):
     # config.repo_dir = PARENT_DIR
 
     OmegaConf.resolve(config)
-    np.random.seed(config.seed)
+    set_seed_everywhere(config.seed)
     os.makedirs(config.out_dir, exist_ok=True)
     global_config = OmegaConf.to_yaml(config)
-    
+
     logger.debug(OmegaConf.to_yaml(config))
     logger.debug(f"Logging to {HydraConfig.get().run.dir}")
 
@@ -45,40 +47,60 @@ def main(config: DictConfig):
         judge = LlamaGuardJudge(config.judge.device)
     else:
         raise NotImplementedError(f"Judge {config.judge.judge_type} not implemented")
-    
+
     logger.debug("Loading dataset")
     goals = get_dataset(**config.dataset_configs)["prompt"]
-    
+
     save_config = OmegaConf.to_yaml(config)
     with open(os.path.join(config.out_dir, "config.yaml"), "w") as f:
         f.write(save_config)
 
-    redteaming_game = Game(
-                config.seed, 
-                attacker,
-                defender,
-                judge,
-                goals, 
-                config.max_turns)
-    
+    redteaming_game = Game(config.seed, attacker, defender, judge, goals, config.max_turns)
+
     trajs = []
     errors = []
     for goal in tqdm(goals):
         try:
             redteaming_game.reset(goal=goal)
-            traj, judge_score = redteaming_game.simulate()
-            trajs.append({"game": traj, "judge": judge_score})
+            env_state, judge_score = redteaming_game.simulate()
+            attacker_value_function_evals, defender_value_function_evals = None, None
+            if config.value_function_evaluate.do_eval:
+                # This function evaluates thre accuracy of the value function prediction for the attacker and defender
+                if config.value_function_evaluate.attacker.evaluate:
+                    attacker_value_function_evals = evaluate_value_function(
+                        conversation=env_state.to_attacker_message(),
+                        value_function_type=config.value_function_evaluate.attacker.value_function_type,
+                        rewards=judge_score["rewards"],
+                        policy_type="attacker",
+                    )
+                if config.value_function_evaluate.defender.evaluate:
+                    defender_value_function_evals = evaluate_value_function(
+                        conversation=env_state.to_defender_message(),
+                        value_function_type=config.value_function_evaluate.defender.value_function_type,
+                        rewards=judge_score["rewards"],
+                        policy_type="defender",
+                    )
+            trajs.append(
+                {
+                    "game": env_state.to_game_message(),
+                    "judge": judge_score,
+                    "attacker_value_function_evals": attacker_value_function_evals,
+                    "defender_value_function_evals": defender_value_function_evals,
+                }
+            )
         except Exception as e:
             logger.error(f"Error in simulating game for goal: {goal}")
             logger.error(e)
             errors.append({"goal": goal, "error": str(e)})
-            slack_notification(f"Error in simulating game for goal: {goal}, config: {global_config}")
-            
-    config.out_fname = os.path.join(config.out_dir,config.out_fname)
+            slack_notification(
+                f"Error in simulating game for goal: {goal}, config: {global_config}"
+            )
+
+    config.out_fname = os.path.join(config.out_dir, config.out_fname)
     write_json(trajs, config.out_fname)
     if len(errors) > 0:
         write_json(errors, config.out_fname.replace(".json", "_errors.json"))
-    
+
 
 if __name__ == "__main__":
     try:
