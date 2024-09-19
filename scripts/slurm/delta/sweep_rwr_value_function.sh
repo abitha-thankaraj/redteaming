698c696880504685e12926c0f5179048d039718a
abitha <athanka@cs.cmu.edu>
2024-09-19 01:38:26 -0400

    sth

---
+++ scripts/slurm/delta/sweep_rwr_value_function.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Array of learning rates
+learning_rates=(1e-6 5e-7)
+# Array of value function experiments
+value_function_experiments=("multilabel" "binary")
+
+# Base master port
+base_master_port=29500
+
+# Path to your main SLURM script
+main_script="/scratch/bcgv/athankaraj/redteaming/scripts/slurm/delta/rwr.sh"
+
+# Loop through learning rates and value function experiments
+for i in "${!learning_rates[@]}"; do
+    for j in "${!value_function_experiments[@]}"; do
+        lr=${learning_rates[$i]}
+        value_fn=${value_function_experiments[$j]}
+        master_port=$((base_master_port + i*10 + j))
+        
+        echo "Submitting job with learning rate: $lr, value function: $value_fn, and master port: $master_port"
+        
+        sbatch $main_script \
+            "meta-llama/Meta-Llama-3.1-8B-Instruct" \
+            "defender" \
+            "$master_port" \
+            "class_balanced" \
+            "prefix" \
+            "$value_fn" \
+            "$lr" \
+            "1.0" \
+            "Meta-Llama-3.1-8B-Instruct_length" \
+            "value_fn_rwr_sweep_${value_fn}_${lr}"
+    done
+done
